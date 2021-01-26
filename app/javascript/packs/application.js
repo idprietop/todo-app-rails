6f42e176ce8c76216165edb4733d5d12ec6164f9
Ivan <ivan.prieto.kommit@gmail.com>
2021-01-26 09:27:56 -0500

    Setup bootstrap framework

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("packs/custom")
+require("packs/posts.js")
 
 Rails.start()
 Turbolinks.start()
